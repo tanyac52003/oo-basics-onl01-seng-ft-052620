ee061a949653f84e309ebf3c819153bbcadac0fa
Tanya Hodges <tanyac52003@gmail.com>
2020-06-04 15:49:26 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,12 @@
 # Make your shoe class here!
 
 class Shoe 
-  attr_accessor :brand 
+  attr_accessor :brand, :properties
+  attr_reader :color, :size, :material, :condition
   
   def initialize(brand)
+    @brand = brand 
   end 
+  
   
 end 
